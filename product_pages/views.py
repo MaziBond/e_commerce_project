77bb88fe9fce170e0de025a12f7ebb9c730f1f62
Sunnysteve <ugwusundaysteve@gmail.com>
2023-04-10 09:41:29 +0000

    - create dashboard and add sidenav to list pages - create new page called get profile with edit button - add button leading to exiting page called edit profile

--- product_pages/views.py
+++ product_pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from user_auth.models import UserProfile
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+
+@login_required(login_url='login')
+def dashboard(request):
+    user = request.user
+    user_profile = get_object_or_404(UserProfile, user=user)
+    context = {
+        'user_profile': user_profile,
+        'user_detail': user
+    }
+    return render(request, 'dashboard/dashboard.html', context)
--- user_auth/views.py
+++ user_auth/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import UserCreationForm, UserLoginForm, UserUpdateForm, UserProfileUpdateForm
 from .models import User, UserProfile
@@ -16,7 +16,7 @@ def register(request):
             return redirect('login')
         else:
             messages.error(request, 'Error registering')
-            return redirect('register')
+            return redirect('signup')
     else:
         form = UserCreationForm()
     return render(request, 'user/register.html', {'form': form})
@@ -32,7 +32,7 @@ def user_login(request):
             if user:
                 print(request.user.is_authenticated)
                 login(request, user)
-                return redirect('home')
+                return redirect('dashboard')
             else:
                 messages.error(request, 'Error logging in')
                 return redirect('login')
@@ -52,8 +52,8 @@ def custom_authenticate(email=None, password=None):
     except Exception as e:
         return None
 
-# @login_required
-def profile(request):
+@login_required(login_url='login')
+def create_profile(request):
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
         profile_form = UserProfileUpdateForm(request.POST, 
@@ -63,7 +63,7 @@ def profile(request):
             user_form.save()
             profile_form.save()
             messages.success(request, 'Your account has been updated successfully')
-            return redirect('profile')
+            return redirect('get-profile')
         else:
             messages.error(request, 'An error occurred while updating your profile')
 
@@ -75,4 +75,18 @@ def profile(request):
         'user_form': user_form,
         'profile_form': profile_form
     }
-    return render(request, 'user/profile.html', context)
+    return render(request, 'user/profile_edit.html', context)
+
+@login_required(login_url='login')
+def get_profile(request):
+    user = request.user
+    user_profile = get_object_or_404(UserProfile, user=user)
+    context = {
+        'user_profile': user_profile,
+        'user_detail': user
+    }
+    return render(request, 'user/profile_detail.html', context)
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
