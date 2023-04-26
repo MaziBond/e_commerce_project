28107d944c59ad4e6c62911d4ceb52f5c24e9772
MaziBond <101129446+MaziBond@users.noreply.github.com>
2023-04-26 16:51:02 +0000

    Update settings.py

--- e_commerce_project/settings.py
+++ e_commerce_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^$%x_q&pes*m@je-08sk-w1xczgl@*(#66z8p!!um_l6tv-ysr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
